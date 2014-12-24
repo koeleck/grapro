b0681ceba773e43836a12d903adc076f141d1012
Philipp Kögel <philippkoegel@web.de>
2014-12-24 23:20:08 +0100

    debug_break() only, when debugger is present

---
+++ src/util/debug.h
@@ -0,0 +1,7 @@
+#ifndef UTIL_DEBUG_H
+#define UTIL_DEBUG_H
+
+extern bool debugger_is_present;
+
+#endif // UTIL_DEBUG_H
+
--- src/util/debugbreak.h
+++ src/util/debugbreak.h
@@ -27,6 +27,24 @@
 #ifndef DEBUG_BREAK_H
 #define DEBUG_BREAK_H
 
+#include "debug.h"
+
+#if defined(NDEBUG)
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__attribute__((gnu_inline, always_inline))
+static void __inline__ debug_break(void)
+{
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#else
+
 #include <signal.h>
 #include <unistd.h>
 #include <sys/syscall.h>
@@ -36,9 +54,9 @@ extern "C" {
 #endif
 
 enum {
-	/* gcc optimizers consider code after __builtin_trap() dead.
-	 * Making __builtin_trap() unsuitable for breaking into the debugger */
-	DEBUG_BREAK_PREFER_BUILTIN_TRAP_TO_SIGTRAP = 0,
+    /* gcc optimizers consider code after __builtin_trap() dead.
+     * Making __builtin_trap() unsuitable for breaking into the debugger */
+    DEBUG_BREAK_PREFER_BUILTIN_TRAP_TO_SIGTRAP = 0,
 };
 
 #if defined(__i386__) || defined(__x86_64__)
@@ -46,7 +64,7 @@ enum { HAVE_TRAP_INSTRUCTION = 1, };
 __attribute__((gnu_inline, always_inline))
 static void __inline__ trap_instruction(void)
 {
-	__asm__ volatile("int $0x03");
+    __asm__ volatile("int $0x03");
 }
 #elif defined(__thumb__)
 enum { HAVE_TRAP_INSTRUCTION = 1, };
@@ -54,38 +72,38 @@ enum { HAVE_TRAP_INSTRUCTION = 1, };
 __attribute__((gnu_inline, always_inline))
 static void __inline__ trap_instruction(void)
 {
-	/* See 'arm-linux-tdep.c' in GDB source.
-	 * Both instruction sequences below works. */
+    /* See 'arm-linux-tdep.c' in GDB source.
+     * Both instruction sequences below works. */
 #if 1
-	/* 'eabi_linux_thumb_le_breakpoint' */
-	__asm__ volatile(".inst 0xde01");
+    /* 'eabi_linux_thumb_le_breakpoint' */
+    __asm__ volatile(".inst 0xde01");
 #else
-	/* 'eabi_linux_thumb2_le_breakpoint' */
-	__asm__ volatile(".inst.w 0xf7f0a000");
+    /* 'eabi_linux_thumb2_le_breakpoint' */
+    __asm__ volatile(".inst.w 0xf7f0a000");
 #endif
 
-	/* Known problem:
-	 * After a breakpoint hit, can't stepi, step, or continue in GDB.
-	 * 'step' stuck on the same instruction.
-	 *
-	 * Workaround: a new GDB command,
-	 * 'debugbreak-step' is defined in debugbreak-gdb.py
-	 * that does:
-	 * (gdb) set $instruction_len = 2
-	 * (gdb) tbreak *($pc + $instruction_len)
-	 * (gdb) jump   *($pc + $instruction_len)
-	 */
+    /* Known problem:
+     * After a breakpoint hit, can't stepi, step, or continue in GDB.
+     * 'step' stuck on the same instruction.
+     *
+     * Workaround: a new GDB command,
+     * 'debugbreak-step' is defined in debugbreak-gdb.py
+     * that does:
+     * (gdb) set $instruction_len = 2
+     * (gdb) tbreak *($pc + $instruction_len)
+     * (gdb) jump   *($pc + $instruction_len)
+     */
 }
 #elif defined(__arm__) && !defined(__thumb__)
 enum { HAVE_TRAP_INSTRUCTION = 1, };
 __attribute__((gnu_inline, always_inline))
 static void __inline__ trap_instruction(void)
 {
-	/* See 'arm-linux-tdep.c' in GDB source,
-	 * 'eabi_linux_arm_le_breakpoint' */
-	__asm__ volatile(".inst 0xe7f001f0");
-	/* Has same known problem and workaround
-	 * as Thumb mode */
+    /* See 'arm-linux-tdep.c' in GDB source,
+     * 'eabi_linux_arm_le_breakpoint' */
+    __asm__ volatile(".inst 0xe7f001f0");
+    /* Has same known problem and workaround
+     * as Thumb mode */
 }
 #else
 enum { HAVE_TRAP_INSTRUCTION = 0, };
@@ -94,20 +112,24 @@ enum { HAVE_TRAP_INSTRUCTION = 0, };
 __attribute__((gnu_inline, always_inline))
 static void __inline__ debug_break(void)
 {
-	if (HAVE_TRAP_INSTRUCTION) {
-		trap_instruction();
-	} else if (DEBUG_BREAK_PREFER_BUILTIN_TRAP_TO_SIGTRAP) {
-		 /* raises SIGILL on Linux x86{,-64}, to continue in gdb:
-		  * (gdb) handle SIGILL stop nopass
-		  * */
-		__builtin_trap();
-	} else {
-		raise(SIGTRAP);
-	}
+    if (debugger_is_present) {
+        if (HAVE_TRAP_INSTRUCTION) {
+            trap_instruction();
+        } else if (DEBUG_BREAK_PREFER_BUILTIN_TRAP_TO_SIGTRAP) {
+             /* raises SIGILL on Linux x86{,-64}, to continue in gdb:
+              * (gdb) handle SIGILL stop nopass
+              * */
+            __builtin_trap();
+        } else {
+            raise(SIGTRAP);
+        }
+    }
 }
 
 #ifdef __cplusplus
 }
 #endif
+
+#endif // NDEBUG
 
 #endif
