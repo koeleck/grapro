46173b224d86ec897c0eccd63a553ac2477a4e28
Philipp Kögel <philippkoegel@web.de>
2015-01-27 22:51:46 +0100

    run.sh: automatically run in gdb, when gl_debug_break == true

--- run.sh
+++ run.sh
@@ -4,8 +4,28 @@ set -e
 
 make
 
+use_gdb=false
+if egrep -q "^ *gl_debug_break += +true" grapro.cfg; then
+    use_gdb=true
+    echo "use gdb"
+fi
+
+config_file="grapro.cfg"
+if [[ -n "$1" ]]; then
+    config_file="$1"
+fi
+echo "Using config file: ${config_file}"
+
 if [[ "$(hostname)" == "pkl03" ]]; then
-    primusrun ./bin/grapro -c grapro.cfg
+    if [[ "$use_gdb" = true ]]; then
+        gdb -ex=r --args primusrun ./bin/grapro -c "${config_file}"
+    else
+        primusrun ./bin/grapro -c "${config_file}"
+    fi
 else
-    ./bin/grapro -c grapro.cfg
+    if [[ "$use_gdb" = true ]]; then
+        gdb -ex=r --args ./bin/grapro -c "${config_file}"
+    else
+        ./bin/grapro -c "${config_file}"
+    fi
 fi
